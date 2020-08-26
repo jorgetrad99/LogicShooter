bf37f84e0304dcb9bd53af96268fcfc86b7ba42f
Daniel Ivan Escobar Vasquez <daanielescobar@gmail.com>
2020-08-25 19:11:38 -0500

    Project setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:{
+        home: path.resolve(__dirname, 'src/index.js')
+    },
+    mode: 'development',
+    output:{
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'index',
+            template: 'src/index.html'
+        })
+    ]
+}
