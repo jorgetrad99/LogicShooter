c301d98977927f1544d5e236d13c55c6a1dff733
Jorge Trad <jetradl@gmail.com>
2020-09-02 01:05:54 -0500

    We can know rotate logic gates propertly

--- src/index.js
+++ src/index.js
@@ -1,38 +1,137 @@
 import { init, Sprite, GameLoop, getContext } from 'kontra';
-import LogicGate, { Enemies } from "./drawings.js"
 import EnergyBar, {LifeBar} from './Bars.js'
 
 let { canvas } = init();
 
-let ctx = getContext('2d');
-
 const CENTER = 400
 
-let and = new LogicGate({
-    x: 250,
-    y: 0
-}, 0)
+let ctx = getContext("2d")
 
-/* let or = new LogicGate({
-    x: 150,
-    y: 0
-}, 1) */
+/* let and = new LogicGate({
+  x: 100, y: 100
+}, 0) */
 
 let bar = new EnergyBar(document.querySelector('.energy-bar'), 100)
 let bar2 = new LifeBar(document.querySelector('.life-bar'), 85) 
 
-var gameObjects = [ and ]
+let and = new logicGate(-350, 0, 0, 0)
+let or = new logicGate(-350, 0,  90, 1)
+var gameObjects = [ and, or, bar, bar2 ]
+
+
 
 /****** FUNCTIONS *///////////
+
+function toRad(angle) {
+  return angle * Math.PI / 180
+}
+
 function circuito() {
   //Circulito
   ctx.beginPath()
-  /* this.ctx.arc() */
+  /* ctx.arc() */
   ctx.arc(CENTER, CENTER, 3, 0, Math.PI * 2, true)
   ctx.fillStyle = "white"
   ctx.closePath()
   ctx.fill()
 }
+/****** LOGIC GATES ******/
+function logicGate(x, y, angle, type){
+  this.r = 18
+  this.x = x
+  this.y = y
+  this.angle = angle
+  this.type = type
+
+  this.update = function(){
+    ctx.fillStyle = "#899"
+    ctx.strokeStyle = "#000"
+    ctx.lineWidth = 3
+    ctx.save();
+    ctx.translate(CENTER, CENTER);
+    ctx.rotate(this.angle);
+
+    ctx.beginPath()
+        
+    //And
+    if(this.type === 0) {
+      //Semicircle
+      
+      ctx.arc(this.x, this.y, this.r, Math.PI / 2, Math.PI * 3 / 2, true)
+      ctx.fill()
+
+      //Rectangle
+      ctx.moveTo(this.x, this.y -  this.r)
+      ctx.lineTo(this.x - this.r, this.y - this.r)
+      ctx.lineTo(this.x - this.r, this.y + this.r)
+      ctx.lineTo(this.x, this.y + this.r)
+      ctx.fill()
+      ctx.stroke()
+
+      //Patitas
+      //Patita superior trasera
+      ctx.beginPath()
+      ctx.moveTo(this.x - this.r, this.y - this.r + this.r / 2)
+      ctx.lineTo(this.x - this.r * 2, this.y - this.r + this.r / 2)
+      ctx.stroke()
+      
+      //Patita inferior trasera
+      ctx.beginPath()
+      ctx.moveTo(this.x - this.r, this.y + this.r / 2)
+      ctx.lineTo(this.x - this.r * 2, this.y + this.r / 2)
+      ctx.stroke()
+
+      //Patita delantera
+      ctx.beginPath()
+      ctx.moveTo(this.x + this.r, this.y)
+      ctx.lineTo(this.x + this.r * 2, this.y)
+      ctx.stroke()   
+      
+        
+    }
+    //Or
+    if( this.type === 1) {
+      //Trazos
+      //curva trasera
+      ctx.arc(this.x - this.r * 2 - Math.sin(Math.PI / 4), 
+          this.y, this.r / Math.sin(Math.PI / 4), 
+          Math.PI / 4, -(Math.PI / 4), true)
+
+      /////////////ESTE ES EL BUENO
+      ctx.arcTo(this.x + this.r, this.y - this.r, this.x + this.r * 2, this.y + this.r, this.r*2)
+      ctx.arcTo(this.x + this.r , this.y + this.r, this.x - this.r *2, this.y + this.r, this.r*2)
+      ctx.closePath()
+      
+      ctx.fill()
+      ctx.stroke()
+      
+      //Patita delantera
+      ctx.beginPath()
+      ctx.moveTo(this.x + this.r * 3/2 + 1, this.y + 2)
+      ctx.lineTo(this.x + this.r * 5/2, this.y +2)
+      ctx.stroke()
+
+      //Patita superior trasera
+      ctx.beginPath()
+      ctx.moveTo(this.x - this.r + 7, this.y - this.r + this.r / 2)
+      ctx.lineTo(this.x - this.r * 2, this.y - this.r + this.r / 2)
+      ctx.stroke()
+      
+      //Patita inferior trasera
+      ctx.beginPath()
+      ctx.moveTo(this.x - this.r + 7, this.y + this.r / 2)
+      ctx.lineTo(this.x - this.r * 2, this.y + this.r / 2)
+      ctx.stroke()
+      
+    }
+    ctx.restore();    
+  }  
+}
+
+
+
+
+
 
 
 
@@ -41,29 +140,16 @@ let loop = GameLoop({  // create the main game loop
     
   update: function() { // update the game state
     /* sprite.update(); */
-    
-    bar.update()
-    bar2.update()
-    /* and.rot() */
-    /* console.log(and.angle) */
-
     gameObjects.forEach((object) => object.update())
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    /* if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
-    } */
+    and.angle += 1 * Math.PI / 180; 
+    or.angle += 1 * Math.PI / 180; 
   },
   render: function() { // render the game state
-    /* or.render() */
-    /* sprite.render(); */
 
-   
-
-    gameObjects.forEach((object) => object.render())
+    gameObjects.forEach((object) => object.update())
     circuito()
   },
-  fps: 1
+  fps: 60
 });
 
 
